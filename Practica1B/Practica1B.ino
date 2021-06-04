f336955b89d1744a7914b9c7baf7a2e2d9e3d922
Almudena Garcia <liberamenso10000@gmail.com>
2021-06-04 03:20:15 +0200

    fix: add delay between analogRead, to avoid esp8266 crash

--- Practica1B/Practica1B.ino
+++ Practica1B/Practica1B.ino
@@ -3,40 +3,48 @@ long tiempo = 0;
 long cuenta = 0;
 
 const int LDR = A0;
-const int MAX_LUZ = 950;
+const int MAX_LUZ = 900;
+const int MIN_LUZ = 800;
 
 
 void setup() {
 
   // Inicialización del RS232
   Serial.begin(9600);
-  // Definición del pulsador
+
+  // Definición del LDR
   pinMode(LDR, INPUT);
 
 }
 
 void loop() {
-  char cadena[50];
+  char cadena[20];
   int rebote;
-
-  Serial.println(analogRead(LDR));
+  int luz = analogRead(LDR);
+  
+  Serial.println(luz);
   
   // Espera hasta que se corte el foco de luz
-  while (analogRead(LDR) >= MAX_LUZ);
+  while (luz >= MAX_LUZ){
+    luz = analogRead(LDR);
+    delay(100); // Se realiza una pequeña espera antes de repetir la lectura
+  }
   
   // Incrementa el contador
   cuenta++;
   tiempo = millis();
   
   // Se genera la cadena a enviar
-  sprintf(cadena, "Cuenta: %d; Tiempo: %lu", cuenta, tiempo);
+  snprintf(cadena, 20, "Cuenta: %d; Tiempo: %lu", cuenta, tiempo);
   Serial.println(cadena);
 
   // Espera a que se estabilice el valor
   rebote = millis();
-  while(millis()-rebote < 20);
+  while(millis() - rebote < 20);
   
   // Se espera a que vuelva a subir la luz
-  while(digitalRead(LDR) < MAX_LUZ);
-
+  while(luz <= MIN_LUZ){
+    luz = analogRead(LDR);
+    delay(100); // Se realiza una pequeña espera antes de repetir la lectura
+  }
 }
